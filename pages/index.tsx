6a3451d35c9c3dd35aa1f8be16eff9e5d9f9d134
João Maranhão <joaofelipemaranhao@gmail.com>
2022-08-09 19:35:23 -0300

    chore: add tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      Home
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
