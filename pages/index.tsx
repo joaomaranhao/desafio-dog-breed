da9fe20707be8b5eab2ab36c18bae5e3e9f9581b
João Maranhão <joaofelipemaranhao@gmail.com>
2022-08-09 20:38:25 -0300

    refactor: add dog icon to register page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import { RegisterForm } from '../components/RegisterForm'
+import { GiSittingDog } from 'react-icons/gi'
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +8,10 @@ const Home: NextPage = () => {
       <div className='max-w-sm mx-auto px-6'>
         <div className='relative flex flex-wrap'>
           <div className='w-full relative'>
-            <div className='mt-6'>
+            <div className='mt-6 flex flex-col'>
+              <div className='m-auto mb-8'>
+                <GiSittingDog size="8rem" />
+              </div>
               <div className="text-center font-semibold text-black">
               Registre-se
               </div>
