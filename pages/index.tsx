c585594783a82a68a9fd20e2bf2c76ebf693bae4
João Maranhão <joaofelipemaranhao@gmail.com>
2022-08-09 20:07:17 -0300

    feat: create Register Page (Home)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
 import type { NextPage } from 'next'
+import { RegisterForm } from '../components/RegisterForm'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className='container max-w-full mx-auto py-24 px-6'>
+      <div className='max-w-sm mx-auto px-6'>
+        <div className='relative flex flex-wrap'>
+          <div className='w-full relative'>
+            <div className='mt-6'>
+              <div className="text-center font-semibold text-black">
+              Registre-se
+              </div>
+              <RegisterForm />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
