354520b2a380cc1cfd0c829473f409b60370b4f2
João Maranhão <joaofelipemaranhhao@gmail.com>
2022-08-11 02:28:27 -0300

    refactor: setCookies with Authorization token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,11 @@
-import { createContext, ReactNode } from 'react'
+import { createContext, ReactNode, useState } from 'react'
+import { setCookie } from 'nookies'
+import { useRouter } from 'next/router'
 import { api } from '../services/api'
+
+type User = {
+  email: string
+}
 
 type SignInCredentials = {
   email: string
@@ -7,6 +13,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
   signIn(credentials: SignInCredentials): Promise<void>
+  user: User | undefined
   isAuthenticated: boolean
 }
 
@@ -17,17 +24,37 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider ({ children }: AuthProviderProps) {
-  const isAuthenticated = false
+  const [user, setUser] = useState<User>()
+  const router = useRouter()
+  const isAuthenticated = !!user
 
   async function signIn ({ email }: SignInCredentials) {
-    const response = await api.post('register', {
-      email
-    })
-    console.log(response.data)
+    try {
+      const response = await api.post('register', {
+        email
+      })
+
+      const { token } = response.data.user
+
+      setUser({
+        email
+      })
+
+      setCookie(undefined, 'dogbreed.token', token, {
+        maxAge: 60 * 60, // 1 hour
+        path: '/'
+      })
+
+      api.defaults.headers.common.Authorization = token
+
+      router.push('/lista')
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
-    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       { children }
     </AuthContext.Provider>
   )
