cfc0076a953491c493bdeb77ead9f445881c524f
João Maranhão <joaofelipemaranhhao@gmail.com>
2022-08-12 02:13:43 -0300

    refactor: add Authorization header with token on api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
+import { parseCookies } from 'nookies'
+
+const cookies = parseCookies()
 
 export const api = axios.create({
-  baseURL: 'https://dogbreed-api.q9.com.br'
+  baseURL: 'https://dogbreed-api.q9.com.br',
+  headers: {
+    Authorization: cookies['dogbreed.token']
+  }
 })
